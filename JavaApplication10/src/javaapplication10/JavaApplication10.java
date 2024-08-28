c9df17f1b28b94de6da029e56e04c31a184d497a
Valentino_Pardo <valentinopardo321@gmail.com>
2024-08-28 02:19:13 -0300

    Add files via upload

--- JavaApplication10/src/javaapplication10/JavaApplication10.java
+++ JavaApplication10/src/javaapplication10/JavaApplication10.java
@@ -5,6 +5,7 @@ public class JavaApplication10 {
 
     public static void main(String[] args) {
         System.out.println("ads adsasd");
+        System.out.println("A nisman lo mataron!!!");
     }
     
 }
