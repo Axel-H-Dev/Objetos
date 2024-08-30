e37e313047f708d1d966dfafc0b159678e6b1b1f
valentino <166979527+Blutvogelspinne@users.noreply.github.com.>
2024-08-29 22:13:10 -0300

    Segundo Commit

--- JavaApplication10/src/javaapplication10/JavaApplication10.java
+++ JavaApplication10/src/javaapplication10/JavaApplication10.java
@@ -4,8 +4,9 @@ package javaapplication10;
 public class JavaApplication10 {
 
     public static void main(String[] args) {
-        System.out.println("ads adsasd");
-        System.out.println("A nisman lo mataron!!!");
+        System.out.println("Prueba 98");
+        System.out.println("prueba 99");
+        System.out.println("Prueba 100!");
     }
     
 }
